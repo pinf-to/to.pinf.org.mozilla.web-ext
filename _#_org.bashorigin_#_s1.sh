16f9dbea6096d3483ddcd67f20a29d0467a082a5
Christoph Dorn <christoph@christophdorn.com>
2017-12-06 22:25:38 -0800

    wip

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash.origin.script
 
 depend {
-    "webext_it": "@com.github/pinf-it/it.pinf.org.mozilla.web-ext#s1"
+    "webext_it": "@com.github/pinf-it/it.pinf.org.mozilla.web-ext#s1",
+    "git": "@com.github/bash-origin/bash.origin.gitscm#s1"    
 }
 
 
 function EXPORTS_run {
-    local runConfig=$(BO_run_recent_node "$__DIRNAME__/lib/builder.js" "$1")
+    local runConfig=$(BO_run_recent_node "$__DIRNAME__/lib/builder.js" "$1" "$(CALL_git get_branch)")
 
     BO_log "$VERBOSE" "[to.pinf.org.mozilla.web-ext] runConfig: $runConfig"
 
